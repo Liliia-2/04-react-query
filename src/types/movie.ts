dd8d496aa3940551e28404e0d72723b73420f972
Liliia <plv-86@ukr.net>
2026-05-12 23:00:09 +0300

    second commit

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,12 +1,12 @@
 export interface Movie {
   id: number;
-  // poster_path: string;
-  // backdrop_path: string;
+  poster_path: string;
+  backdrop_path: string;
   title: string;
   overview: string;
-  // release_date: string;
-  // vote_average: number;
-  poster_path: string;
+  release_date: string;
+  vote_average: number;
+  
 }
 export interface MoviesResponse {
   page: number;
