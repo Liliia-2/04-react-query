4c632d4a17ee7ce9c605b8045fecd92081c64ce3
Liliia Pastushenko <plv-86@ukr.net>
2026-05-12 21:14:55 +0300

    Initial commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import type { Movie } from "../types/movie";
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+if (!TOKEN) {
+  throw new Error("TMDB token is missing");
+}
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const response = await axios.get<FetchMoviesResponse>(BASE_URL, {
+    params: {
+      query,
+    },
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  return response.data.results;
+};
